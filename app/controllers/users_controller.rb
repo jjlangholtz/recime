20355d656226cd4e52b4e0db8b2fc97b6cc5e5d7
Joshua Langholtz <jjlangholtz@gmail.com>
2014-07-03 01:11:54 -0400

    Added secure authentication

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,15 +10,17 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to root_path
+      sign_in @user
+      flash[:success] = "Welcome to Recime!"
+      redirect_to @user
     else
-      render :new
+      render 'new'
     end
   end
 
   private
 
   def user_params
-    params.require(:user).permit(:name, :email)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  root 'static_pages#home'
+
+  resource :session, only: [:new, :create, :destroy]
   resources :users, only: [:show, :new, :create]
 end
